5ac3f06c1d5a1369ba4921cdd273ad63bbfe0cc6
Atech158 <anshukreddy13@gmail.com>
2025-12-10 15:28:21 +0530

    Add Firebase messaging service worker file

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,35 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+apiKey: "AIzaSyD8Q-Ru_QGobJ_VbOW31karDMj6r3GGCdE",
+  authDomain: "scanbell-ar.firebaseapp.com",
+  projectId: "scanbell-ar",
+  storageBucket: "scanbell-ar.firebasestorage.app",
+  messagingSenderId: "1093927523954",
+  appId: "1:1093927523954:web:3cc412e7a98e27c05bd4eb"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
+
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
